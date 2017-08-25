fcf5455d52856b4993a9212d3f0800840ce4f663
Davis <dzabiyaka1@gmail.com>
2017-08-25 12:36:20 -0400

    our user beans

--- home/src/main/java/com/revature/beans/Tournament.java
+++ home/src/main/java/com/revature/beans/Tournament.java
@@ -2,10 +2,81 @@ package com.revature.beans;
 
 import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
 public class Tournament {
 
+	@Min(value=1)
+	@Id
+	@Column(name="TOURNAMENT_ID")
 	private int id;
+	@NotBlank
+	@Column
 	private String name;
+	@Column
+	@ManyToOne
 	private Coach coach;
+	@Column
+	@ManyToMany
 	private Set<Player> participants;
+	
+	public Tournament() {
+		super();
+	}
+
+	public Tournament(int id, String name, Coach coach, Set<Player> participants) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.coach = coach;
+		this.participants = participants;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Coach getCoach() {
+		return coach;
+	}
+
+	public void setCoach(Coach coach) {
+		this.coach = coach;
+	}
+
+	public Set<Player> getParticipants() {
+		return participants;
+	}
+
+	public void setParticipants(Set<Player> participants) {
+		this.participants = participants;
+	}
+
+	@Override
+	public String toString() {
+		return "Tournament [id=" + id + ", name=" + name + ", coach=" + coach + ", participants=" + participants + "]";
+	}
+	
+	
 }
