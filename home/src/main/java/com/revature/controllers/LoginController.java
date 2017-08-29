bbd741e5135e6468e88a8fa347cc5415f838a141
Davis <dzabiyaka1@gmail.com>
2017-08-29 14:52:59 -0400

    please work entitites

--- home/src/main/java/com/revature/controllers/LoginController.java
+++ home/src/main/java/com/revature/controllers/LoginController.java
@@ -1,13 +1,9 @@
 package com.revature.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class LoginController {
@@ -24,12 +20,6 @@ public class LoginController {
 	@RequestMapping(value="login", method=RequestMethod.POST)
 	public void testLogin(){
 		System.out.println("Here");
-	}
-	
-	@RequestMapping(value="login", method=RequestMethod.POST)
-	public String getHomePageAfterLogin(HttpServletRequest request) {
-		System.out.println(request.getParameter("username"));
-		return "redirect:/pages/home.html";
 	}
 
 }
