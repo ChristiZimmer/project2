8a6e993619d89f0460b32a57a80fc0e527e4a01a
ChristiZimmer <christi_zimmer@yahoo.com>
2017-08-31 16:26:05 -0400

    can create a tournament

--- home/src/main/webapp/static/controllers.js
+++ home/src/main/webapp/static/controllers.js
@@ -6,12 +6,21 @@ angular.module("atp").controller("rankingController", function($http, $scope) {
 	// do something
 });
 
-angular.module("atp").controller("tournamentController", function($http, $scope) {
+angular.module("atp").controller("tournamentGetAllController", function($http, $scope) {
 	$http({
 		method: "GET", url: "/home/tournament/all"
 	}).then(function(response) {
 		$scope.allTournaments = response.data;
 	});
+});
+
+angular.module("atp").controller("tournamentCreateController", function($http, $scope) {
+	$scope.createTournament = function(){
+		$http.post("/home/tournament/create", $scope.tournament)
+		.then(function(value) {
+			window.alert("Tournament Added");
+		})
+	}
 });
 
 angular.module("atp").controller("logoutController", function($http, $scope) {
