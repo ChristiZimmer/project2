9e449374b1a0ac5404dfb1dd7d97c1fff2a862cc
Davis <dzabiyaka1@gmail.com>
2017-08-31 14:53:25 -0400

    loginctrl change

--- home/src/main/java/com/revature/controllers/LoginController.java
+++ home/src/main/java/com/revature/controllers/LoginController.java
@@ -1,7 +1,5 @@
 package com.revature.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
